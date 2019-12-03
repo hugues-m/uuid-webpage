581077dfe44c70d7950949c10c8db170d04a1a89
Hugues Maignol <hugues@hmlb.fr>
2019-12-03 08:57:36 +0100

    Updates

---
+++ precache-manifest.bd9a17fd5df36710f2230ac22b999f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d189fd3b5eadeef8f82d523d986a1b63",
+    "url": "/uuid-webpage/index.html"
+  },
+  {
+    "revision": "72879e9aae2cb5e21b05",
+    "url": "/uuid-webpage/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "bb0c0182d833f303a0ee",
+    "url": "/uuid-webpage/static/js/2.22c9782e.chunk.js"
+  },
+  {
+    "revision": "72879e9aae2cb5e21b05",
+    "url": "/uuid-webpage/static/js/main.5973e136.chunk.js"
+  },
+  {
+    "revision": "baa6447561f10cbc5d9b",
+    "url": "/uuid-webpage/static/js/runtime-main.82357ed0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/uuid-webpage/static/media/logo.25bf045c.svg"
+  }
+]);
