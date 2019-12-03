7a3b8baf069233fea769807f81adcef32c45d506
Hugues Maignol <hugues@hmlb.fr>
2019-12-03 12:11:30 +0100

    Updates

--- precache-manifest.fab191d6bc81c7e3e5cb6c5dbdc82c19.js
+++ precache-manifest.fab191d6bc81c7e3e5cb6c5dbdc82c19.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "261f3df231443489fb175eb5df7875ed",
+    "revision": "936e2c8158ce4be1de16d6c41444de86",
     "url": "/index.html"
   },
   {
-    "revision": "478856e558c5be45acc1",
-    "url": "/static/css/main.a70490c0.chunk.css"
+    "revision": "594cbd9fe681575ac2ba",
+    "url": "/static/css/main.ce1639ae.chunk.css"
   },
   {
     "revision": "2013435fd07e87d69263",
     "url": "/static/js/2.0f567c03.chunk.js"
   },
   {
-    "revision": "478856e558c5be45acc1",
-    "url": "/static/js/main.7ab9c7df.chunk.js"
+    "revision": "594cbd9fe681575ac2ba",
+    "url": "/static/js/main.0fd5ce5d.chunk.js"
   },
   {
     "revision": "ecaf9cd460d33dc8775f",
